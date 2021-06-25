3ebe56c01f00456301a972466f9578ddb68b33cb
Arunram07 <arunram@zevocorp.in>
2021-06-25 16:10:55 +0530

    fix : modal components ui is changed

--- src/components/Modals/Modals.stories.tsx
+++ src/components/Modals/Modals.stories.tsx
@@ -5,7 +5,7 @@ import { Modals, ModalsProps } from "./Modals";
 
 
 export default {
-  title: "Modals",
+  title: "Widgets/Modal",
   component: Modals,
   argTypes: {
     // onClick: { action: "clicked" },
@@ -16,11 +16,25 @@ const Template: Story<ModalsProps> = (args) => <Modals {...args} />;
 
 export const Modal1 = Template.bind({});
 Modal1.args = {
-    modalHead: "Payment Processing",
+    title: "Payment Processing",
     modalDesc : "Your payment is processing. It usually takes few seconds to complete the payment. Please be patient if its taking a bit longer than we expected.",
-    modalFoot1 : "Authenticating your payment",
-    headImg: "",
-    footImg1: "",
+    modalFooter : "Authenticating your payment",
     error: false,
 };
 
+export const Modal2 = Template.bind({});
+Modal2.args = {
+    title: "Payment Complete",
+    variant:"complete",
+    modalDesc : "Congratulations, your betting for 2X Roulette is complete. You can continue with your roll now.",
+    modalFooter : "Secure paymet by Metamask",
+    error: false,
+};
+export const Modal3 = Template.bind({});
+Modal3.args = {
+    title: "Payment Incomplete",
+    variant:"incomplete",
+    modalDesc : "We’re sorry, your payment couldnot take place. It might be due to some technical error. Please retry now or try again sometime later.",
+    modalFooter : "Error code #1233",
+    error: true,
+};
--- src/components/Modals/Modals.tsx
+++ src/components/Modals/Modals.tsx
@@ -1,39 +1,48 @@
 import React from "react";
 import "./modals.css";
+import paymentProcessing from '../../assets/icons/paymentProcessing.svg'
+import paymentIncomplete from '../../assets/icons/paymentIncomplete.svg'
+import paymentComplete from '../../assets/icons/paymentComplete.svg'
+import processImg from '../../assets/icons/Gif.svg'
+import check_green from '../../assets/icons/check_green.svg'
+import errorImg from '../../assets/icons/error.svg'
+import refresh from '../../assets/icons/refresh.svg'
 
 export interface ModalsProps {
-  modalHead: string;
+  title: string;
+  variant:String,
   modalDesc: string;
-  modalFoot1: string;
-  headImg: string;
-  footImg1: string;
+  modalFooter: string;
   error: boolean
   onClick?: () => void;
 }
 
 export const Modals: React.FC<ModalsProps> = ({
-  modalHead,
-  headImg,
+  title,
+  variant,
   modalDesc,
-  modalFoot1,
-  footImg1,
+  modalFooter,
   error,
   
   ...props
 }) => {
+
+  const titleImg=variant==="incomplete"?paymentIncomplete:variant==="complete"?paymentComplete:paymentProcessing;
+  const footerImg=variant==="incomplete"?errorImg:variant==="complete"?check_green:processImg;
   return (
     <div className="modalContainer">
       <div className="modalHeader">
-        <img className="modalImg" src={headImg} alt="headImg" />
-        <h1>{modalHead}</h1>
+        <img className="modalImg" src={titleImg} alt="headImg" style={{width:26}} />
+        <h1>{title}</h1>
       </div>
       <div className="modalBody">
         <p>{modalDesc}</p>
       </div>
       <div className="modalFooter">
-        <img className="modalImg" src={footImg1} alt="footImg1" />
-        {!error? <p>{modalFoot1}</p> : <p style={{color:"#F65151"}}>{modalFoot1}</p>}
+        <img className="modalImg" src={footerImg} alt="footImg1" />
+        {!error? <p>{modalFooter}</p> : <p style={{color:"#F65151"}}>{modalFooter}</p>}
       </div>
+      {error&&<p className="retry_btn"><img src={refresh} alt="refresh" style={{width:20,marginRight:8}} /><span>Retry</span></p>}
     </div>
   );
 };
